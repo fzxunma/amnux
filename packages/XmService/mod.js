f2d34e53ea1ae4ad34b9365d5f5a00543ad7b311
linzuxin <linzuxin@163.com>
2025-12-22 14:31:48 +0800

    ui

--- packages/XmService/mod.js
+++ packages/XmService/mod.js
@@ -1,5 +1,5 @@
 export { XmMap } from "./map/XmMap.js";
 export { XmExcelDownload } from "./excel/XmEecelDownload.js";
 export { XmExcelUpload } from "./excel/XmExcelUpload.js";
-export { AliOssSts } from "./oss/AliOssSts.js";
+export { AliOssSts } from "./oss/aliosssts.js";
 export { XmSmsAliyun } from "./alisms/XmSmsAliyun.js";
