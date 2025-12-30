924d2d4df7ebad7f9dbd0dadb1ee1ec2541ef912
linzuxin <linzuxin@163.com>
2025-12-30 17:32:15 +0800

    menu

--- packages/XmUI/pages/web/home/XmSubRight.vue
+++ packages/XmUI/pages/web/home/XmSubRight.vue
@@ -4,6 +4,7 @@ import {
     NLayoutSider,
     NMenu,
 } from 'naive-ui'
+import { menuRightOptions } from "./XmSubMenu"
 import XmSubDrawer from './XmSubDrawer.vue'
 import XmLayoutEditor from '/components/layout/XmLayoutEditor.vue'
 
@@ -17,7 +18,7 @@ const menuChange = (key, value) => {
 </script>
 <template>
     <NLayoutSider bordered collapse-mode="width" :collapsed-width="64" :width="240" :collapsed="true">
-        <NMenu :options="menuOptions" :collapsed-width="64" :collapsed-icon-size="22"
+        <NMenu :options="menuRightOptions" :collapsed-width="64" :collapsed-icon-size="22"
             @update:value="(key, value) => menuChange(key, value)" />
     </NLayoutSider>
     <n-drawer v-model:show="drawerVisible" width="640">
